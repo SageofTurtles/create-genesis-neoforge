ccee183f68747495ab4429345283eacd76150ce1
SageofTurtles <mkern1200@gmail.com>
2026-05-15 14:45:02 -0500

    Tweaked mob drops
    Bats drop leather, shulkers always drop 2 shells, ender dragon drops a dragon head & dragon egg

---
+++ common/kubejs/server_scripts/loot_tables.js
@@ -0,0 +1,19 @@
+LootJS.lootTables(event => {
+  // ENTITY DROPS
+  event.getLootTable('minecraft:entities/bat')
+    .firstPool()
+    .addEntry(LootEntry.of('minecraft:leather').applyEnchantmentBonus([0, 1]))
+
+  event.getLootTable('minecraft:entities/shulker')
+    .clear()
+    .firstPool()
+    .addEntry(LootEntry.of('minecraft:shulker_shell', 2))
+
+  event.getLootTable('minecraft:entities/ender_dragon')
+    .createPool(pool => {
+      pool.addEntry('minecraft:dragon_head')
+    })
+    .createPool(pool => {
+      pool.addEntry('minecraft:dragon_egg')
+    })
+})
