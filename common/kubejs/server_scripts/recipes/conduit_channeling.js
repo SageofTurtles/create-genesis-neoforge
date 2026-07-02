2cc0d0168dedd345f5e7526f38755e96cfa40582
SageofTurtles <mkern1200@gmail.com>
2026-07-01 20:43:35 -0500

    Renamed channeling recipes and added oxidization for Create copper blocks

--- common/kubejs/server_scripts/recipes/conduit_channeling.js
+++ common/kubejs/server_scripts/recipes/conduit_channeling.js
@@ -9,11 +9,19 @@ ServerEvents.recipes(event => {
         count: outputCount,
         chance: outputChance
       }]
-    }).id(`kubejs:${recipeId}_aquatic_channeling`)
+    }).id(`kubejs:${recipeId}_conduit_channeling`)
   }
 
-  // RECIPE CHANGES
+  // TARGETED RECIPE CHANGES
   add('bone', 'minecraft:bone', 'minecraft:turtle_scute', 0.5, 1)
   add('egg', 'minecraft:egg', 'minecraft:turtle_egg', 0.1, 1)
   add('limestone', 'create:limestone', 'minecraft:nautilus_shell', 0.02, 1)
+
+  // BULK RECIPE CHANGES
+  global.COPPER_BLOCKSETS_OXIDIZATION.forEach(entry => {
+    const { base, exposed, weathered, oxidized } = entry
+    add(`${base}_to_${exposed}`, `create:${base}`, `create:${exposed}`, 1.0, 1)
+    add(`${exposed}_to_${weathered}`, `create:${exposed}`, `create:${weathered}`, 1.0, 1)
+    add(`${weathered}_to_${oxidized}`, `create:${weathered}`, `create:${oxidized}`, 1.0, 1)
+  })
 })
