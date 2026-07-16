6a2574f785c026838c56e4acc3bfcdb035c1ce65
SageofTurtles <mkern1200@gmail.com>
2026-07-15 22:34:11 -0500

    Added recipe to craft budding amethyst

--- common/kubejs/server_scripts/recipes/filling.js
+++ common/kubejs/server_scripts/recipes/filling.js
@@ -8,6 +8,7 @@ ServerEvents.recipes(event => {
   }
 
   // RECIPE CHANGES
+  add('budding_amethyst', Fluid.ingredientOf('create:potion', { 'create:potion_fluid_bottle_type': 'regular', 'minecraft:potion_contents': { potion: 'minecraft:strong_regeneration' } }).withAmount(1000), 'minecraft:amethyst_block', 'minecraft:budding_amethyst')
   add('ink_sac', Fluid.of('create_dragons_plus:black_dye', 250), 'kubejs:empty_ink_sac', 'minecraft:ink_sac')
   add('podzol', Fluid.of('sliceanddice:fertilizer', 250), 'minecraft:dirt', 'minecraft:podzol')
   add('pointed_dripstone', Fluid.water(500), 'minecraft:clay_ball', 'minecraft:pointed_dripstone')
