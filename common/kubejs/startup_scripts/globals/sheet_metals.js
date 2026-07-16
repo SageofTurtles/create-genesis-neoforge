c0969924b958ebf990f82d8d4a828b314ca4b9eb
SageofTurtles <mkern1200@gmail.com>
2026-07-15 22:24:31 -0500

    Standardized recipes for Design 'n' Decor sheet metals

---
+++ common/kubejs/startup_scripts/globals/sheet_metals.js
@@ -0,0 +1,34 @@
+global['SHEET_METALS'] = [
+  {
+    sheet: 'create:brass_sheet',
+    block: 'dndecor:brass_sheet_metal'
+  },
+  {
+    sheet: 'createdeco:andesite_sheet',
+    block: 'dndecor:andesite_sheet_metal'
+  },
+  {
+    sheet: 'createaddition:zinc_sheet',
+    block: 'dndecor:zinc_sheet_metal'
+  },
+  {
+    sheet: 'create:copper_sheet',
+    block: 'dndecor:copper_sheet_metal'
+  },
+  {
+    sheet: 'createdeco:industrial_iron_sheet',
+    block: 'dndecor:industrial_sheet_metal'
+  },
+  {
+    sheet: 'create:golden_sheet',
+    block: 'dndecor:gold_sheet_metal'
+  },
+  {
+    sheet: 'create:iron_sheet',
+    block: 'dndecor:iron_sheet_metal'
+  },
+  {
+    sheet: 'createvintageneoforged:netherite_sheet',
+    block: 'dndecor:netherite_sheet_metal'
+  }
+]
