06c10e915afe437e1b5764f6419c8e6c44cc824a
SageofTurtles <mkern1200@gmail.com>
2026-07-15 22:41:23 -0500

    Standardized Macaw's Roofs recipes

---
+++ common/kubejs/startup_scripts/globals/wood_roofs.js
@@ -0,0 +1,98 @@
+global['WOOD_ROOFS'] = [
+  {
+    type: 'oak',
+    material: 'minecraft:oak_log'
+  },
+  {
+    type: 'spruce',
+    material: 'minecraft:spruce_log'
+  },
+  {
+    type: 'birch',
+    material: 'minecraft:birch_log'
+  },
+  {
+    type: 'jungle',
+    material: 'minecraft:jungle_log'
+  },
+  {
+    type: 'acacia',
+    material: 'minecraft:acacia_log'
+  },
+  {
+    type: 'dark_oak',
+    material: 'minecraft:dark_oak_log'
+  },
+  {
+    type: 'crimson',
+    material: 'minecraft:crimson_stem'
+  },
+  {
+    type: 'warped',
+    material: 'minecraft:warped_stem'
+  },
+  {
+    type: 'mangrove',
+    material: 'minecraft:mangrove_log'
+  },
+  {
+    type: 'cherry',
+    material: 'minecraft:cherry_log'
+  },
+  {
+    type: 'bamboo',
+    material: 'minecraft:bamboo_block'
+  },
+  {
+    type: 'oak_planks',
+    material: 'minecraft:oak_planks'
+  },
+  {
+    type: 'spruce_planks',
+    material: 'minecraft:spruce_planks'
+  },
+  {
+    type: 'birch_planks',
+    material: 'minecraft:birch_planks'
+  },
+  {
+    type: 'jungle_planks',
+    material: 'minecraft:jungle_planks'
+  },
+  {
+    type: 'acacia_planks',
+    material: 'minecraft:acacia_planks'
+  },
+  {
+    type: 'dark_oak_planks',
+    material: 'minecraft:dark_oak_planks'
+  },
+  {
+    type: 'crimson_planks',
+    material: 'minecraft:crimson_planks'
+  },
+  {
+    type: 'warped_planks',
+    material: 'minecraft:warped_planks'
+  },
+  {
+    type: 'mangrove_planks',
+    material: 'minecraft:mangrove_planks'
+  },
+  {
+    type: 'cherry_planks',
+    material: 'minecraft:cherry_planks'
+  },
+  {
+    type: 'bamboo_planks',
+    material: 'minecraft:bamboo_planks'
+  },
+  {
+    type: 'bamboo_mosaic',
+    material: 'minecraft:bamboo_mosaic'
+  },
+  {
+    type: 'thatch',
+    material: 'minecraft:hay_block'
+  }
+]
