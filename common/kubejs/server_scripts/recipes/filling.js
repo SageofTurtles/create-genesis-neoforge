8a94aed7a9934a4e3a07aab1e491c5bbe382a93a
SageofTurtles <mkern1200@gmail.com>
2026-07-31 14:44:03 -0500

    Added recipe for Farmer's Delight wild plants

--- common/kubejs/server_scripts/recipes/filling.js
+++ common/kubejs/server_scripts/recipes/filling.js
@@ -7,11 +7,18 @@ ServerEvents.recipes(event => {
     ).id(`kubejs:${recipeId}_filling`)
   }
 
-  // RECIPE CHANGES
+  // TARGETED RECIPE CHANGES
   add('budding_amethyst', Fluid.ingredientOf('create:potion', { 'create:potion_fluid_bottle_type': 'regular', 'minecraft:potion_contents': { potion: 'minecraft:strong_regeneration' } }).withAmount(1000), 'minecraft:amethyst_block', 'minecraft:budding_amethyst')
   add('ink_sac', Fluid.of('create_dragons_plus:black_dye', 250), 'kubejs:empty_ink_sac', 'minecraft:ink_sac')
   add('podzol', Fluid.of('sliceanddice:fertilizer', 250), 'minecraft:dirt', 'minecraft:podzol')
   add('pointed_dripstone', Fluid.water(500), 'minecraft:clay_ball', 'minecraft:pointed_dripstone')
   add('spore_blossom', Fluid.of('sliceanddice:fertilizer', 250), 'minecraft:big_dripleaf', 'minecraft:spore_blossom')
   add('wither_rose', Fluid.ingredientOf('create:potion', { 'create:potion_fluid_bottle_type': 'regular', 'minecraft:potion_contents': { potion: 'minecraft:harming' } }).withAmount(250), Ingredient.of('#minecraft:flowers'), 'minecraft:wither_rose')
+
+  // BULK RECIPE CHANGES
+  global.WILD_PLANTS.forEach(entry => {
+    const { crop, wild } = entry
+    let name = crop.split(':')[1]
+    add(name, Fluid.of('sliceanddice:fertilizer', 100), crop, wild)
+  })
 })
---
+++ common/kubejs/startup_scripts/globals/wild_plants.js
@@ -0,0 +1,34 @@
+global['WILD_PLANTS'] = [
+  {
+    crop: 'minecraft:dead_bush',
+    wild: 'farmersdelight:sandy_shrub'
+  },
+  {
+    crop: 'farmersdelight:cabbage',
+    wild: 'farmersdelight:wild_cabbages'
+  },
+  {
+    crop: 'farmersdelight:onion',
+    wild: 'farmersdelight:wild_onions'
+  },
+  {
+    crop: 'farmersdelight:tomato',
+    wild: 'farmersdelight:wild_tomatoes'
+  },
+  {
+    crop: 'farmersdelight:rice_panicle',
+    wild: 'farmersdelight:wild_rice'
+  },
+  {
+    crop: 'minecraft:carrot',
+    wild: 'farmersdelight:wild_carrots'
+  },
+  {
+    crop: 'minecraft:potato',
+    wild: 'farmersdelight:wild_potatoes'
+  },
+  {
+    crop: 'minecraft:beetroot',
+    wild: 'farmersdelight:wild_beetroots'
+  }
+]
